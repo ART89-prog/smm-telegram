4eb37813a44035d3947ac1b84d609bcf83227a00
ART89-prog <72185684+ART89-prog@users.noreply.github.com>
2021-12-18 11:59:34 +0300

    Правки3
    Правки3

--- js/my.js
+++ js/my.js
@@ -12,38 +12,7 @@ $(document).ready(function(){
     });
 
 
-// niceSelect
-    $('select').niceSelect();
 
-
-    // $('select').niceSelect();
-    $.fn.allTheClasses = function() {
-        return this[0].className.split(' ').slice(1,this[0].length);
-    }
-    $('.nice-select .option').click(function () {
-        var newClass = $(this).allTheClasses();
-        $(this).parents('.nice-select').find('.current').attr('class', 'current ' + newClass);
-    });
-
-    tippy('[data-tippy-content]', {
-        theme: 'light',
-        animation: 'scale',
-        maxWidth: 220
-    });
-
-    tippy('.social-list__btn--other', {
-        theme: 'light',
-        animation: 'scale',
-        maxWidth: 275,
-        trigger: 'click',
-        placement: 'left-start',
-        content(reference) {
-            const id = reference.getAttribute('data-template');
-            const template = document.getElementById(id);
-            return template.innerHTML;
-        },
-        allowHTML: true,
-    });
 
 
 	// Аккордион
@@ -64,7 +33,6 @@ $(document).ready(function(){
 	})
 
 
-
-       
+      
 
 });
